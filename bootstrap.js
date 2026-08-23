452d9b6b142d5969d7bdfbde02f4d1c9dc713f17
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 22:26:09 -0400

    Enable V0.10 performance manager

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,4 @@
 await import("./world-enhancements.js?v=20260822-10");
 await import("./player-controller.js?v=20260822-10");
 await import("./game.js?v=20260822-10");
+await import("./performance-manager.js?v=20260822-10");
