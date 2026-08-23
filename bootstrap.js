5322766759f59250b2e6f32c8c67459336d4a536
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 23:09:37 -0400

    Rollback broken camera and sign changes to V0.11

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,5 @@
-await import("./world-enhancements.js?v=20260822-15");
-await import("./player-controller-v2.js?v=20260822-15");
-await import("./game.js?v=20260822-15");
-await import("./precision-collision-v2.js?v=20260822-15");
-await import("./sign-upgrade.js?v=20260822-15");
-await import("./performance-manager.js?v=20260822-15");
+await import("./world-enhancements.js?v=20260822-16");
+await import("./player-controller.js?v=20260822-16");
+await import("./game.js?v=20260822-16");
+await import("./precision-collision.js?v=20260822-16");
+await import("./performance-manager.js?v=20260822-16");
