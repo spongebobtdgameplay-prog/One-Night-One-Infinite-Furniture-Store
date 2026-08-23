dbeb4e0cd9f128b1983e38640efa4c58e5ef693f
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 22:55:23 -0400

    Bump corrected player controls build

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,6 @@
-await import("./world-enhancements.js?v=20260822-12");
-await import("./player-controller.js?v=20260822-12");
-await import("./player-polish.js?v=20260822-12");
-await import("./game.js?v=20260822-12");
-await import("./precision-collision-v2.js?v=20260822-12");
-await import("./performance-manager.js?v=20260822-12");
+await import("./world-enhancements.js?v=20260822-13");
+await import("./player-controller.js?v=20260822-13");
+await import("./player-polish.js?v=20260822-13");
+await import("./game.js?v=20260822-13");
+await import("./precision-collision-v2.js?v=20260822-13");
+await import("./performance-manager.js?v=20260822-13");
