d5186d8100eed3a2333476b6006cbdfa2920d32c
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 22:08:49 -0400

    Load V0.09 player corrections before game startup

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,4 @@
-await import("./world-enhancements.js?v=20260822-8");
-await import("./player-controller.js?v=20260822-8");
-await import("./game.js?v=20260822-8");
+await import("./world-enhancements.js?v=20260822-9");
+await import("./player-controller.js?v=20260822-9");
+await import("./player-fixes.js?v=20260822-9");
+await import("./game.js?v=20260822-9");
