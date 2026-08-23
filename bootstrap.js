391899cc5cc64c99e11e987c6bbb7195aca18c31
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 22:25:14 -0400

    Load clean V0.10 runtime

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,3 @@
-await import("./world-enhancements.js?v=20260822-9");
-await import("./player-controller.js?v=20260822-9");
-await import("./player-fixes.js?v=20260822-9");
-await import("./game.js?v=20260822-9");
+await import("./world-enhancements.js?v=20260822-10");
+await import("./player-controller.js?v=20260822-10");
+await import("./game.js?v=20260822-10");
