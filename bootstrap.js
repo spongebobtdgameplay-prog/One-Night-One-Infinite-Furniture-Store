4ac7e724ba90db720780c1c4d1e46f327975559a
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 23:07:14 -0400

    Use single eye anchored camera controller

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,6 @@
-await import("./world-enhancements.js?v=20260822-14");
-await import("./player-controller.js?v=20260822-14");
-await import("./player-polish.js?v=20260822-14");
-await import("./game.js?v=20260822-14");
-await import("./precision-collision-v2.js?v=20260822-14");
-await import("./performance-manager.js?v=20260822-14");
+await import("./world-enhancements.js?v=20260822-15");
+await import("./player-controller-v2.js?v=20260822-15");
+await import("./game.js?v=20260822-15");
+await import("./precision-collision-v2.js?v=20260822-15");
+await import("./sign-upgrade.js?v=20260822-15");
+await import("./performance-manager.js?v=20260822-15");
