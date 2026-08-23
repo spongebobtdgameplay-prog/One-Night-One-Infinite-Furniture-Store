68e6e0aba0f5d4b17b8eb0fdd1ec79e2e14895a3
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 21:57:41 -0400

    Load V0.08 player and endless store systems

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,3 @@
-await import("./world-enhancements.js?v=20260822-7");
-await import("./player-controller.js?v=20260822-7");
-await import("./game.js?v=20260822-7");
+await import("./world-enhancements.js?v=20260822-8");
+await import("./player-controller.js?v=20260822-8");
+await import("./game.js?v=20260822-8");
