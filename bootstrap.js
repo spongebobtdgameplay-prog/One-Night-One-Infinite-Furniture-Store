cbb0b13ddf6ebedd31145c2109c7e6455886e70d
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 22:51:05 -0400

    Load V0.12 camera animation and geometry fixes

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,6 @@
-await import("./world-enhancements.js?v=20260822-11");
-await import("./player-controller.js?v=20260822-11");
-await import("./game.js?v=20260822-11");
-await import("./precision-collision.js?v=20260822-11");
-await import("./performance-manager.js?v=20260822-11");
+await import("./world-enhancements.js?v=20260822-12");
+await import("./player-controller.js?v=20260822-12");
+await import("./player-polish.js?v=20260822-12");
+await import("./game.js?v=20260822-12");
+await import("./precision-collision-v2.js?v=20260822-12");
+await import("./performance-manager.js?v=20260822-12");
