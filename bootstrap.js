a236c2e21aa72b1d5befb3801ddb45a834a699d8
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 21:42:03 -0400

    Load player controller before game

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,3 @@
-await import("./world-enhancements.js?v=20260822-6");
-await import("./game.js?v=20260822-6");
+await import("./world-enhancements.js?v=20260822-7");
+await import("./player-controller.js?v=20260822-7");
+await import("./game.js?v=20260822-7");
