f950d6850c5158591f468a7184d1d07db33c5ea1
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-22 23:15:43 -0400

    Load focused V0.11 camera collision and sign fixes

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,8 @@
-await import("./world-enhancements.js?v=20260822-16");
-await import("./player-controller.js?v=20260822-16");
-await import("./game.js?v=20260822-16");
-await import("./precision-collision.js?v=20260822-16");
-await import("./performance-manager.js?v=20260822-16");
+await import("./world-enhancements.js?v=20260822-17");
+await import("./player-controller.js?v=20260822-17");
+await import("./camera-fix.js?v=20260822-17");
+await import("./game.js?v=20260822-17");
+await import("./precision-collision-v2.js?v=20260822-17");
+await import("./collision-cleanup.js?v=20260822-17");
+await import("./sign-fix.js?v=20260822-17");
+await import("./performance-manager.js?v=20260822-17");
