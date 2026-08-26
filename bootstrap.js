8c4814d444d103212b8a67bba4c7c88d34e657ee
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-26 16:49:21 -0400

    Bump account and security client build

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260826-117";
-const Version = "0.25.7";
+const Cache = "20260826-123";
+const Version = "0.25.8";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
