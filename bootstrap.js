de5ab68e202a73f70c8e5135d4b8ab72f3558cd8
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-26 16:23:32 -0400

    Bump responsive multiplayer client build

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260826-99";
-const Version = "0.25.2";
+const Cache = "20260826-104";
+const Version = "0.25.4";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
