26fd416c33650a729be8cff1596f17c7c89376cd
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-27 17:22:45 -0400

    Bump store build for box and collision fixes

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260826-156";
-const Version = "0.27.7";
+const Cache = "20260827-157";
+const Version = "0.27.8";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
