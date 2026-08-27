5cbfe335e3c2fe60063bfef29f75eace4bd5ef8d
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-26 22:23:51 -0400

    Bump mixed-density kitchen build

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260826-155";
-const Version = "0.27.6";
+const Cache = "20260826-156";
+const Version = "0.27.7";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
