10af54099046cd329233756d5647de33ca77bdbe
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-29 19:29:22 -0400

    Load bathroom sink and shelf snap build

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260829-physics7";
-const Version = "0.28.0";
+const Cache = "20260829-bathroom1";
+const Version = "0.28.1";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
