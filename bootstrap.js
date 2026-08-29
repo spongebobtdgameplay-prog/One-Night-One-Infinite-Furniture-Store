081b0bc646df2f14484ed60d8b751bc9885c57d3
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-29 19:15:48 -0400

    Load per-foot grounding physics build

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260829-physics5";
-const Version = "0.27.8";
+const Cache = "20260829-physics6";
+const Version = "0.27.9";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
