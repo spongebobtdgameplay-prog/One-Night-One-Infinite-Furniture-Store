20058d1f919b679bcb4c9abcef8198355b17aae5
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-30 09:58:59 -0400

    Bump store build to 0.35.19

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260830-v03518-curb1";
-const Version = "0.35.18";
+const Cache = "20260830-v03519-quality1";
+const Version = "0.35.19";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
