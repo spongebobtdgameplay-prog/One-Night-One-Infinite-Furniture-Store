7edc7630f16bad923985d6fb07574929ee7b8320
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-29 22:54:19 -0400

    Expose V0.35.14 latched ledge state

--- game.js
+++ game.js
@@ -1302,8 +1302,8 @@ const PlacementApi = {
   ShapeCastPlacement
 };
 
-window.__STORE_GAME_BUILD__ = "V0.35.13";
-window.__STORE_VERSION__ = "0.35.13";
+window.__STORE_GAME_BUILD__ = "V0.35.14";
+window.__STORE_VERSION__ = "0.35.14";
 window.__STORE_GAME__ = {
   Scene,
   Camera,
@@ -1324,6 +1324,6 @@ window.__STORE_GAME__ = {
   SetWorldSeed,
   Placement: PlacementApi,
   RayCollisionMode: true,
-  Version: "0.35.13"
+  Version: "0.35.14"
 };
 Animate();
