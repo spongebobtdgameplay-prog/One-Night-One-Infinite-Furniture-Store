4dccbf829eebe90ff707eef5e1065080f6bfdb10
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-29 21:07:58 -0400

    Mark V0.35.6 preload and seed build

--- loading-prewarm-r38.js
+++ loading-prewarm-r38.js
@@ -104,7 +104,7 @@ function ResolveWorldSeed() {
 const World = ResolveWorldSeed();
 window.__STORE_WORLD_SEED__ = World.Seed;
 window.__STORE_WORLD_SEED_SOURCE__ = World.Source;
-window.__STORE_WORLD_SEED_BUILD__ = "V0.35.3-SEED";
+window.__STORE_WORLD_SEED_BUILD__ = "V0.35.6-SEED";
 
 function CreateLoaderUi() {
   if (!BootCard) return { SkipButton: null, Warning: null, Progress: null, SeedLabel: null };
@@ -254,4 +254,4 @@ LoaderUi.Wrapper?.remove();
 
 window.__STORE_PRELOAD_PROMISES__ = AssetPromises;
 window.__STORE_PRELOAD_RESULT__ = Result;
-window.__STORE_PRELOAD_BUILD__ = "V0.35.3-CORNER";
+window.__STORE_PRELOAD_BUILD__ = "V0.35.6-TRIANGLE";
