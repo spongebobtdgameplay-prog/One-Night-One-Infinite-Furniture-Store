349a1c3990175434f3a7014582440c2d515f888a
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-29 23:12:21 -0400

    Load V0.35.15 streaming performance rewrite

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260830-v03514-latch1";
-const Version = "0.35.14";
+const Cache = "20260830-v03515-stream2";
+const Version = "0.35.15";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
