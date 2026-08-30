b2764e94d10e6d3e14b6d3abddcb508d9dfbb524
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-29 22:14:35 -0400

    Load V0.35.11 resolved movement animation

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260830-v03510-ledge1";
-const Version = "0.35.10";
+const Cache = "20260830-v03511-motion1";
+const Version = "0.35.11";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
