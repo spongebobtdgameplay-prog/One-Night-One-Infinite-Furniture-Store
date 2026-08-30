45e4c9a219d44432b4aa516889c929056ec9e188
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-30 19:46:54 -0400

    Refresh cache for full-body carpet force

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260830-v03530-foothull1";
-const Version = "0.35.30";
+const Cache = "20260830-v03531-bodyforce1";
+const Version = "0.35.31";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
