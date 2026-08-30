e46c9f928ab020fe73949a16947e5b6c6ba2b67c
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-29 22:24:12 -0400

    Load V0.35.12 ledge foot-lock build

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260830-v03511-motion1";
-const Version = "0.35.11";
+const Cache = "20260830-v03512-footlock1";
+const Version = "0.35.12";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
