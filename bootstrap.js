8b165332105bcba16fe7424defead1aea5951ec1
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-30 20:35:26 -0400

    Refresh cache for 0.35.34

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260830-v03533-exactshoe1";
-const Version = "0.35.33";
+const Cache = "20260830-v03534-boxperfclip1";
+const Version = "0.35.34";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
