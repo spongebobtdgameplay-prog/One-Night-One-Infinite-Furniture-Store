e0e77e59ba6e76b9720ae88c8c501ee3f5cfceb5
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-30 21:38:32 -0400

    Serve 0.35.37 performance fixes

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260830-v03536-loadertruth1";
-const Version = "0.35.36";
+const Cache = "20260830-v03537-fps1";
+const Version = "0.35.37";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
