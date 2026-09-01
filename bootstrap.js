66b35bf3dc8bd0ade402f09a6aabb527afbb26da
spongebobtdgameplay-prog <spongebobtdgameplay@gmail.com>
2026-08-31 20:18:16 -0400

    Serve clean retry build 0.35.44

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
-const Cache = "20260831-v03541-startauthority2";
-const Version = "0.35.41";
+const Cache = "20260831-v03544-cleanretry1";
+const Version = "0.35.44";
 const FaviconVersion = "20260824-4";
 const FaviconLinks = [
   { rel: "icon", type: "image/png", sizes: "32x32", href: `favicon_io/favicon-32x32.png?v=${FaviconVersion}` },
